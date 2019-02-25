21f3d99e475d7695185d8713147bb2c2a78a13c8
AlexWilliams <alex.williams2000@hotmail.co.uk>
2019-02-25 23:52:25 +0000

    Fix index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  console.log('Hey there');
+  const me = {name: 'Alex', age: 100, cool: true };
+  // res.json(me);
+  // res.send(req.query);
+  res.render('hello', {
+    name: 'me',
+    dog: req.query.dog ? req.query.dog : 'tiddles the bull mastiff',
+    title: 'I love food!'
+  });
+});
+ 
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+
 });
 
 module.exports = router;
