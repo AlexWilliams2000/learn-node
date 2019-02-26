38fbc065a6e38dbef769bd87141cd03d71ea40b0
AlexWilliams <alex.williams2000@hotmail.co.uk>
2019-02-26 01:47:38 +0000

    Add middleware to root endpoint

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,13 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Al';
+  res.cookie('name', 'Wes is cool', { maxAge: 9000000 });
+  if (req.name === 'Al') {
+    throw Error('That\'s a stupid name');
+  }
+  next();
+}
+
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render('index');
 }
