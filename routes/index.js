4fe368b465d68f9bb993839e875fd1253e6706f7
AlexWilliams <alex.williams2000@hotmail.co.uk>
2019-02-26 00:16:52 +0000

    Update index.js to use new template

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', (req, res) => {
-  console.log('Hey there');
-  const me = {name: 'Alex', age: 100, cool: true };
-  // res.json(me);
-  // res.send(req.query);
-  res.render('hello', {
-    name: 'me',
-    dog: req.query.dog ? req.query.dog : 'tiddles the bull mastiff',
-    title: 'I love food!'
-  });
-});
- 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
