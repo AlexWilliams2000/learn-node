df0eb5818ad26e407591bbf85b6ae2440c0fa397
AlexWilliams <alex.williams2000@hotmail.co.uk>
2019-03-08 17:07:16 +0000

    Add routes and handlers for store updating

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -27,10 +27,39 @@ exports.createStore = async (req, res) => {
 
   console.log('It worked!');
 
+    // The 'old skool' pre-ES8 approach to promises with error handling:
+    // somePromise
     // .then(storeDetails => storeDetails.find())
     // .then(stores => res.render('storeList', { stores }))
     // .catch((err) => {
     //   throw Error(err);
     // });
   // res.json(req.body);
+  // Error handling is now defined in one place with a wrapper function around createStore added in the router,
+  // see errorHandlers.js and index.js
+};
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  // console.log(stores);
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confirm user owns the store
+  // TODO
+  // 3. Render out the edit form so the user can update it
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // Find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true }).exec();
+  // Redirect user to the store and confirm success
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store ➡️</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+
 };
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,12 @@ const storeController = require('../controllers/storeController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
+// Colon below used to access URL params, see editStore in storeController
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
