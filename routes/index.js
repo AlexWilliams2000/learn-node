f98c04bfca68f52758b298475e7fc19aba17e8e8
AlexWilliams <alex.williams2000@hotmail.co.uk>
2019-05-01 01:32:55 +0100

    Add tags page routes

--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,24 @@ const { catchErrors } = require('../handlers/errorHandlers');
 const router = express.Router();
 
 router.get('/', catchErrors(storeController.getStores));
-router.get('/stores', catchErrors(storeController.getStores));
+
 router.get('/add', storeController.addStore);
+router.post('/add',
+storeController.upload,
+catchErrors(storeController.resize),
+catchErrors(storeController.createStore)
+);
+router.post('/add/:id', 
+storeController.upload,
+catchErrors(storeController.resize),
+catchErrors(storeController.updateStore)
+);
+
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
-router.post('/add',
-  storeController.upload,
-  catchErrors(storeController.resize),
-  catchErrors(storeController.createStore)
-);
-  
-router.post('/add/:id', 
-  storeController.upload,
-  catchErrors(storeController.resize),
-  catchErrors(storeController.updateStore)
-);
-
-// Colon below used to access URL params, see editStore in storeController
-router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 module.exports = router;
