91b5bb9d3f886d400a4adadd181528a251438141
AlexWilliams <alex.williams2000@hotmail.co.uk>
2019-05-04 20:03:05 +0100

    Add middleware to toggle store hearts fpr a given user

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const User = mongoose.model('User');
 const multer = require('multer');
 const jimp = require('jimp');
 const uuid = require('uuid');
@@ -128,7 +129,7 @@ exports.searchStores = async (req, res) => {
       $search: req.query.q
     }
   }, {
-    score: { $meta: 'textScore' }``
+    score: { $meta: 'textScore' }
   }).sort({
     score: { $meta: 'textScore' }
   }).limit(5);
@@ -156,4 +157,14 @@ exports.mapStores = async (req, res) => {
 
 exports.mapPage = (req, res) => {
   res.render('map', { title: 'Map' });
-}
+};
+
+exports.heartStore = async (req, res) => {
+  const hearts = req.user.hearts.map(obj => obj.toString());
+  const operator = hearts.includes(req.params.id) ? '$pull' : '$addToSet';
+  const user = await User.findByIdAndUpdate(req.user._id,
+    { [operator]: { hearts: req.params.id } },
+    { new: true }
+  );
+  res.json(user);
+};
